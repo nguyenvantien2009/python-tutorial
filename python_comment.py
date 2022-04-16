0012e48fcd0556dbe3429ddf128252dbaf0a1ae5
Tien Nguyen <nguyenvantien2009@gmail.com>
2022-04-16 00:59:12 -0500

    Add - Python Comment

---
+++ python_comment.py
@@ -0,0 +1,15 @@
+# Python Code comments
+
+# Comment one line
+
+# Comment block code multiple line code
+#  import numpy as np
+
+
+""" You are also use \"
+To command multiple line code
+"""
+print("You can see 2 methods to comment code")
+print("1. Use symbol #")
+print("2. Use symbol \"")
+
